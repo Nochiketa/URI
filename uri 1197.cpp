cf0cec0c3f295de882d7b221facd7b0ab072fd3b
Nochiketa <chknochiketa.mir@gmail.com>
2016-10-14 16:01:17 +0600

    Add files via upload

---
+++ uri 1197.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int v,t,s;
+    while(scanf("%d %d", &v, &t)!=EOF)
+    {
+        s=2*v*t;
+        cout<<s<<endl;
+    }
+    return 0;
+}
